5e45349805fcceabe658d4e7dc77872c341a6cc2
Asror <asrorrustmaovich007@gmail.com>
2025-03-18 20:19:50 +0500

    asdas

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,45 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Authorizatsiya.views import *
 from Product.views import *
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.routers import DefaultRouter
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API Title",
+      default_version="v1",
+      description="Your API description",
+      terms_of_service="https://example.com/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+)
+
+router = DefaultRouter()
+router.register(r'addresses-of-users', AdressesOfUsersViewSet, basename='addresses-of-users')
 
 urlpatterns = [
      path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),  # DRF Router
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
+    # Authorization
+    path('api/get-otp/', telefon_raqam_uchun_code_genaratsiya, name='get_otp'),
+    path('api/send-otp/', authenticate_user, name='send_otp'),
+
+    # Contacts
+    path('api/contacts/', ContactsListCreateView.as_view(), name='contacts-list-create'),
+
+    # Filials
     path('api/filials/', FilialsListCreateView.as_view(), name='filials-list-create'),
-    path('api/filials/<int:pk>/', FilialsDetailView.as_view(), name='filials-detail'),
 
-    
-    # Product urls
+    # Product
     path('product/', ProducListCreateView.as_view()),
-      # Orders
+
+    # Orders
     path("orders/", OrderListCreateView.as_view()),
     path("orders/<int:pk>/", OrderRetrieveUpdateDeleteView.as_view()),
 
@@ -27,7 +53,8 @@ urlpatterns = [
     # Address API
     path('api/address/create/', AdressUserCreateView.as_view(), name='address-create'),
     path('api/address/list/', AdressUserListView.as_view(), name='address-list'),
-    path("api/adresses_of_user/",AdressesOfUsersViewSet.as_view(),name="adresses_of_user"),
+    path('api/adresses_of_user/', AdressesOfUsersViewSet.as_view({'get': 'list', 'post': 'create'}), name="adresses_of_user"),
+
 
     # Including router URLs
     path('api/', include(router.urls)),

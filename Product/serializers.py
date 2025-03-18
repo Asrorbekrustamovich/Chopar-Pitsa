327c28a423e919398856d4efa6356d5ac642d9bb
Asror <asrorrustmaovich007@gmail.com>
2025-03-18 15:33:11 +0500

    asdasd

---
+++ Product/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import Order, OrderItem, OrderAdditionalItem, ProductPrice, AdditionalProduct, Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'image', 'description']
+
+
+class ProductPriceSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)  
+
+    class Meta:
+        model = ProductPrice
+        fields = ['id', 'product', 'price', 'size']
+
+
+class AdditionalProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdditionalProduct
+        fields = ['id', 'name', 'image', 'price']
+
+
+class OrderAdditionalItemSerializer(serializers.ModelSerializer):
+    additional_product = AdditionalProductSerializer(read_only=True)  
+
+    class Meta:
+        model = OrderAdditionalItem
+        fields = ['id', 'additional_product', 'quantity']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product_price = ProductPriceSerializer(read_only=True)  
+    additional_items = OrderAdditionalItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product_price', 'quantity', 'additional_items']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)  # User ning faqat ismi chiqadi
+    items = OrderItemSerializer(many=True, read_only=True)  
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'total_price', 'status', 'items']

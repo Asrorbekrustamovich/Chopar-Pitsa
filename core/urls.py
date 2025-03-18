4c18f02eae8c663832ace480e9cfe2864cf7fb71
Asror <asrorrustmaovich007@gmail.com>
2025-03-18 20:19:16 +0500

    asdas

--- core/urls.py
+++ core/urls.py
@@ -6,26 +6,7 @@ from Product.views import *
 
 urlpatterns = [
      path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  # DRF Router
-     path('api/get-otp/', telefon_raqam_uchun_code_genaratsiya, name='get_otp'),
-    path('api/send-otp/', registratsiya, name='send_otp'),  # OTP yuborish endpointi
-    path('api/verify-otp/', login, name='verify_otp'),
-     path('api/cities/', CityListCreateView.as_view(), name='city-list-create'),
-    path('api/cities/<int:pk>/', CityDetailView.as_view(), name='city-detail'),
 
-    # Delivery Type
-    path('api/delivery-types/', DeliveryTypeListCreateView.as_view(), name='delivery-type-list-create'),
-    path('api/delivery-types/<int:pk>/', DeliveryTypeDetailView.as_view(), name='delivery-type-detail'),
-
-    # Address for Delivery
-    path('api/addresses/', AddressForDeliveryListCreateView.as_view(), name='address-list-create'),
-    path('api/addresses/<int:pk>/', AddressForDeliveryDetailView.as_view(), name='address-detail'),
-
-    # Contacts
-    path('api/contacts/', ContactsListCreateView.as_view(), name='contacts-list-create'),
-    path('api/contacts/<int:pk>/', ContactsDetailView.as_view(), name='contacts-detail'),
-
-    # Filials
     path('api/filials/', FilialsListCreateView.as_view(), name='filials-list-create'),
     path('api/filials/<int:pk>/', FilialsDetailView.as_view(), name='filials-detail'),
 
